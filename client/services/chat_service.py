f9c646b98eb89a6d48aadbc10448dda9eea944c0
jgarcia <johannagp9@uma.es>
2022-01-25 00:42:04 +0100

    Update chat_service.py

--- client/services/chat_service.py
+++ client/services/chat_service.py
@@ -9,3 +9,20 @@ def get_all_chats(token, params={}):
     if response:
         return response_2_dict(response)
 
+
+def get_chat(id, token):
+    url = APP_NAME + reverse(CHAT_DETAIL_SERVER_ROUTE_NAME, args=(id,)) 
+    params = {}
+    response = generate_request(url, token=token, params=params)
+    if response:
+        return response_2_dict(response)
+
+def update_chat(id, chat, token):
+    url = APP_NAME + reverse(CHAT_DETAIL_SERVER_ROUTE_NAME, args=(id,)) 
+    response = generate_put(url, chat, token=token)
+    return response
+
+def create_chat(chat, token):
+    url = APP_NAME + reverse(CHATS_SERVER_ROUTE_NAME)
+    response = generate_post(url, chat, token=token)
+    return response
