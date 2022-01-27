0753f674730292cb23e69a8038fbe66876769758
LAPTOP-NDH45O55\kmilk <kkamil.koberzynski@gmail.com>
2022-01-27 21:06:48 +0100

    Adding file functionality

--- client/forms.py
+++ client/forms.py
@@ -6,8 +6,12 @@ class CreateGameForm(forms.Form):
         "required": "This field cannot be empty",
         "max_length": "Too long name",
     })
+    user_image = forms.ImageField()
     description = forms.CharField(label="Description", widget=forms.Textarea, max_length="300")
+
 
 class GameInformationForm(forms.Form):
     description_information = forms.CharField(label="Description", widget=forms.Textarea, max_length="300")
+    user_image_2 = forms.ImageField()
+
 
