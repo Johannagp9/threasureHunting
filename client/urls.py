26d6599a12b0c185ded6aa682fe694c9cc20f4b4
kkobierzynski <kkamil.kobierzynski@gmail.com>
2022-01-31 08:41:45 +0100

    New url for edit site

--- client/urls.py
+++ client/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("create", views.create_game),
     path("create/information", views.game_information),
+    path("edit", views.edit_game),
     path('auth/', views.auth_user, name='auth'),
     path('register/', views.auth_user, name='register'),
     path('maps/', views.maps, name='maps'),
